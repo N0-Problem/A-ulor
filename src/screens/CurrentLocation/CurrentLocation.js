64b0f182e682afcda8e0bd90de5f7b95fd6430bf
LeeTaeGyu <dnjsqls2008@gmail.com>
2022-08-24 21:39:41 +0900

    Upload data to server

--- src/screens/CurrentLocation/CurrentLocation.js
+++ src/screens/CurrentLocation/CurrentLocation.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, Platform, PermissionsAndroid } from 'react-native';
+import { View, Text, StyleSheet, Platform, PermissionsAndroid, ActivityIndicator } from 'react-native';
 import { Button } from 'react-native-paper';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import firestore from '@react-native-firebase/firestore';
 import { firebase } from '@react-native-firebase/auth';
 
+// 얘는 메인이나 스플래시 뜰 때 넣어야 할 듯
 async function requestPermission() {
     try {
         if (Platform.OS == "android") {
@@ -21,7 +22,7 @@ async function requestPermission() {
 let centers;
 let queries = []
 
-// firestore에서 이동지원센터 데이터 읽기
+//firestore에서 이동지원센터 데이터 읽기
 // firestore()
 //     .collection('Centers')
 //     .get()
@@ -29,26 +30,28 @@ let queries = []
 //         centers = querySnapshot;
 //     });
 
-// for batch write
-// const db = firebase.firestore();
-// const batch = firestore().batch();
+//for batch write
+const db = firebase.firestore();
+const batch = firestore().batch();
 
-// function batchWrite() {
-//     let docref = db.collection('Centers').doc();
-//     queries.forEach((doc) => {
-//         batch.set(docref, doc);
-//     });
-//     console.log("ready to commit!");
-// }
+// query list에서 하나씩 batchwrite
+function batchWrite() {
+    queries.forEach((doc) => {
+        let docref = db.collection('Operating_time').doc();
+        batch.set(docref, doc);
+    });
+    console.log("ready to commit!");
+}
 
-// function batchCommit() {
-//     batch.commit().then(() => console.log("Success."));
-// }
+function batchCommit() {
+    batch.commit().then(() => console.log("Success."));
+}
 
 function CurrentLocation() {
     const [location, setLocation] = useState();
-
+    
     useEffect(() => {
+        // 위치 정보 권한 획득
         requestPermission().then(result => {
           console.log({result});
           if (result === 'granted') {
@@ -67,6 +70,7 @@ function CurrentLocation() {
             );
           }
         });
+        //batchWrite();
       }, []);
 
       return (
@@ -104,18 +108,12 @@ function CurrentLocation() {
                             }}
                         />
                     </MapView>
-                    <Button>
-
-                    </Button>
+                    {/*<Button title='Commit' onPress={batchCommit()}>Commit</Button>*/}
                 </View>
             ) : (
                 <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <Text>Waiting</Text>
+                    style={styles.container}>
+                    <ActivityIndicator size="large" color="#85DEDC" />
                 </View>
             )}
         </>
