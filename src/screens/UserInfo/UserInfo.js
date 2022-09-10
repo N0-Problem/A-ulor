512db5e4a636fc24ebc3da4499ed6deed0d9c0fc
DongYeon Kang <myjjue00@gmail.com>
2022-09-11 02:50:13 +0900

    Modify UserInfo.js

--- src/screens/UserInfo/UserInfo.js
+++ src/screens/UserInfo/UserInfo.js
@@ -6,10 +6,15 @@ import DropDownPicker from 'react-native-dropdown-picker';
 import auth from '@react-native-firebase/auth';
 
 export default function UserInfo({navigation}) {
-    const [date, setDate] = useState(new Date());
+    // Convert UTC to KST (UTC + 9시간)
+    const curr = new Date();
+    const utc = curr.getTime() + (curr.getTimezoneOffset() * 60 * 1000);
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const today = new Date(utc + (KR_TIME_DIFF));
+
+    const [date, setDate] = useState(today);
     const [open, setOpen] = useState(false);
     const [userName, setUserName] = useState("");
-    const [isNewUser, setIsNewUser] = useState(true);
     const [dropopen, setdropOpen] = useState(false);
     const [dropvalue, setdropValue] = useState(null);
     const [extraInput, setextraInput] = useState(false);
@@ -20,85 +25,116 @@ export default function UserInfo({navigation}) {
         {label: '임산부', value: '임산부'},
         {label: '기타', value: '기타'}
     ]);
+    const [isNewUser, setIsNewUser] = useState(true);
+    const [firstDate, setfirstDate] = useState(false);
 
     useEffect(() => {
         auth().onAuthStateChanged(user => {
             setUserName(user.displayName);
             console.log(userName);
         });
+        //user 정보 받아오기
+        //isNewUser인지 확인
+        // setDate(today);
+        // setdropValue('임산부');
     }, []);
 
+    function date_to_string(d) {
+        let d_year = d.getFullYear();
+        let d_month = d.getMonth();
+        let d_date = d.getDate();
+
+        console.log(String(d_year)+'년 '+String(d_month)+'월 '+String(d_date)+'일');
+        return(String(d_year)+'년 '+String(d_month)+'월 '+String(d_date)+'일');
+    }
     return (
-        // <KeyboardAwareScrollView
-        //     behavior={'height'}
-        //     enabled = {true}>
-            <View style={styles.container}>
-                <View style={styles.input_container}>
-                    <Text style={styles.text_title}>이름</Text>
-                    <Text style={styles.text_input}>{userName}</Text>
-                </View>
-                <View style={styles.input_container}>
-                    <Text style={styles.text_title}>생년월일</Text>
-                    {isNewUser ? (
-                        <TouchableOpacity
-                            onPress={() => setOpen(true)}>
-                            <Text style={styles.text_input}>{date.toDateString()}</Text>
-                        </TouchableOpacity>
-                    ):(
-                        <TouchableOpacity
-                            onPress={() => setOpen(true)}>
-                            <Text style={styles.text_input}>{date.toDateString()}</Text>
-                        </TouchableOpacity>
-                    )}
-                </View>
-                <DatePicker
-                    modal
-                    mode={"date"}
-                    open={open}
-                    date={date}
-                    maximumDate={date}
-                    onConfirm={(date) => {
-                        setOpen(false)
-                        setDate(date)
+        <View style={styles.container}>
+            <View style={styles.input_container}>
+                <Text style={styles.text_title}>이름</Text>
+                <Text style={styles.text_input}>{userName}</Text>
+            </View>
+            <View style={styles.input_container}>
+                <Text style={styles.text_title}>생년월일</Text>
+                {isNewUser ? (
+                    <TouchableOpacity
+                        onPress={() => setOpen(true)}>
+                        {firstDate ? (
+                            <Text style={styles.text_input}>{date_to_string(date)}</Text>
+                        ):(
+                            <Text style={styles.before_text_input}>{'생년월일을 입력해주세요.'}</Text>
+                        )}      
+                    </TouchableOpacity>
+                ):(
+                    <TouchableOpacity
+                        onPress={() => setOpen(true)}>
+                        <Text style={styles.text_input}>{date_to_string(date)}</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+            <DatePicker
+                modal
+                locale='ko'
+                mode={"date"}
+                open={open}
+                date={date}
+                maximumDate={date}
+                onConfirm={(date) => {
+                    setOpen(false)
+                    setfirstDate(true)
+                    setDate(date)
+                }}
+                onCancel={() => {
+                    setOpen(false)
+                }}
+            />
+            <View style={styles.drop_container}>
+                <Text style={styles.drop_title}>특이사항</Text>
+                <DropDownPicker
+                    style={styles.dropdown}
+                    textStyle={{
+                        fontSize: 20,
+                        fontFamily : 'NanumSquare_0',
+                        color : '#454545',
                     }}
-                    onCancel={() => {
-                        setOpen(false)
+                    dropDownContainerStyle={{
+                        width : '98%',
+                        right : 9, 
+                        borderTopColor : '#fff',
+                        borderColor : '#777',
+                    }}
+                    placeholder="특이사항을 선택하세요."
+                    placeholderStyle={{
+                        color: "grey",
+                    }}
+                    open={dropopen}
+                    value={dropvalue}
+                    items={dropitems}
+                    setOpen={setdropOpen}
+                    setValue={setdropValue}
+                    setItems={setdropItems}
+                    onSelectItem={(item) => {
+                        if (item.label === "기타"){
+                            setextraInput(true);
+                        }else {
+                            setextraInput(false);
+                        }
                     }}
                 />
-                <View style={styles.drop_container}>
-                    <Text style={styles.drop_title}>특이사항</Text>
-                    <DropDownPicker
-                        style={styles.dropdown}
-                        open={dropopen}
-                        value={dropvalue}
-                        items={dropitems}
-                        setOpen={setdropOpen}
-                        setValue={setdropValue}
-                        setItems={setdropItems}
-                        onSelectItem={(item) => {
-                            if (item.label === "기타"){
-                                setextraInput(true);
-                            }else {
-                                setextraInput(false);
-                            }
-                        }}
-                    />
-                    {extraInput ? (
-                    <TextInput
-                        label=''
-                        placeholder='기타 특이사항을 입력하세요.'
-                        // value={'입력받기'}
-                        style={styles.extra_input}
-                    />):(<></>)}
-                </View>
-                <Button 
-                    style={styles.button}
-                    color={'#2d2d2d'}
-                    onPress={()=>navigation.navigate('Mypage')}
-                >저장
-                </Button>
+                {extraInput ? (
+                <TextInput
+                    label=''
+                    placeholder='기타 특이사항을 입력하세요.'
+                    // value={'입력받기'}
+                    style={styles.extra_input}
+                />):(<></>)}
             </View>
-        // </KeyboardAwareScrollView>
+            <Button 
+                style={styles.button}
+                color={'#2d2d2d'}
+                onPress={()=>navigation.navigate('Mypage')}
+            >저장
+            </Button>
+        </View>
     )
 }
 
@@ -122,8 +158,20 @@ const styles = StyleSheet.create({
         top: -3,
     },
     text_input: {
-        backgroundColor: '#d4d4d4',
+        backgroundColor: '#e2e2e2',
         color : '#454545',
+        fontFamily : 'NanumSquare_0',
+        fontSize : 22, 
+        height : 40,
+        marginRight : 10,
+        marginTop : 3,
+        textAlignVertical : 'center',
+        paddingLeft: 5, 
+        borderRadius : 7
+    },
+    before_text_input: {
+        backgroundColor: '#e2e2e2',
+        color : '#888',
         fontFamily : 'NanumSquare_0',
         fontSize : 22, 
         height : 40,
@@ -149,7 +197,7 @@ const styles = StyleSheet.create({
         marginTop: 5,
     },
     dropdown : {
-        backgroundColor : '#d4d4d4',
+        backgroundColor : '#e2e2e2',
         borderColor : '#fff',
         height : 43,
         right : 3,
@@ -157,7 +205,7 @@ const styles = StyleSheet.create({
         width : '99%',
     },
     extra_input : {
-        backgroundColor: '#d4d4d4',
+        backgroundColor: '#e2e2e2',
         color : '#454545',
         fontFamily : 'NanumSquare_0',
         textAlignVertical : 'center',
