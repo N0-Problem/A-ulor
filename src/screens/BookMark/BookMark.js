cc43d21be261dfb5b1a56169a59bec2156aa9631
LeeTaeGyu <dnjsqls2008@gmail.com>
2022-09-13 17:39:31 +0900

    Add useIsFocused for re-render

--- src/screens/BookMark/BookMark.js
+++ src/screens/BookMark/BookMark.js
@@ -11,6 +11,7 @@ export default function BookMark({ navigation, route }) {
     let user_id = '';
     const [bookmarks, setBookmarks] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [changed, setChanged] = useState(true);
     const isFocused = useIsFocused();
 
 
@@ -28,11 +29,11 @@ export default function BookMark({ navigation, route }) {
     }
     
     const getBookmarks = async () => {
-        console.log(user_id);
+        console.log('getBookmarks');
         await firestore().collection('Users').doc(user_id).get()
         .then(async (querySnapshot) => {
             if (querySnapshot.exists) {
-                console.log(querySnapshot);
+                // console.log(querySnapshot);
                 centerIds = querySnapshot.data().bookmarks;
                 const centerRef = firestore().collection('Centers');
                 for (const center_id of centerIds) {
@@ -42,12 +43,13 @@ export default function BookMark({ navigation, route }) {
                             for (const doc of querySnapshot.docs) {
                                 if (doc.exists) {
                                     temp.push(doc.data());
-                                    console.log(doc.data());
+                                    // console.log(doc.data());
                                 }
                             }
                         }
                     })
                 }
+                //console.log(temp);
             }
         }).then(() => {
             setBookmarks(temp);
@@ -58,7 +60,7 @@ export default function BookMark({ navigation, route }) {
         const FieldValue = firebase.firestore.FieldValue;
         const docRef = firestore().collection('Users').doc(user_id);
         docRef.update({bookmarks: FieldValue.arrayRemove(centerId)});
-        setLoading(true);
+        setChanged(old => !old);
     }
 
     /// accordion 관련 코드
@@ -132,10 +134,8 @@ export default function BookMark({ navigation, route }) {
         getBookmarks();
         setTimeout(() => {
             setLoading(false);
-            console.log(bookmarks);
-            console.log(temp);
         }, 800);
-    }, [loading, isFocused])
+    }, [changed, isFocused])
 
     if (loading) {
         return (
@@ -153,7 +153,7 @@ export default function BookMark({ navigation, route }) {
     return (
         <View style={styles.container}>
             <View style={styles.title}>
-                <Text style={styles.title_font}>즐겨찾기한 센터</Text>
+                <Text style={styles.title_font}>자주 사용하는 센터</Text>
             </View>
             {                
                 (bookmarks.length > 0) ? (
